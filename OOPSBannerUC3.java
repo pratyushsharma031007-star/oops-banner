7ef5ecfb7787debd916d09631791c5923b12c68a
pratyushsharma031007-star <pratyushsharma031007@gmail.com>
2026-02-23 08:29:28 +0530

    Refactor OOPSBannerUC3 for cleaner output

--- OOPSBannerUC3.java
+++ OOPSBannerUC3.java
@@ -1,53 +1,45 @@
 public class OOPSBannerUC3 {
     public static void main(String[] args) {
 
-        System.out.println(String.join("   ",
+        String line1 = String.join("   ",
                 " ***** ",
                 " ***** ",
-                " ******",
+                "*****  ",
                 " ***** "
-        ));
+        );
 
-        System.out.println(String.join("   ",
+        String line2 = String.join("   ",
                 "*     *",
                 "*     *",
-                "*     *",
+                "*    * ",
                 "*     *"
-        ));
+        );
 
-        System.out.println(String.join("   ",
+        String line3 = String.join("   ",
                 "*     *",
                 "*     *",
-                "*     *",
-                "*     *"
-        ));
-
-        System.out.println(String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *"
-        ));
-
-        System.out.println(String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *"
-        ));
-
-        System.out.println(String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *"
-        ));
-
-        System.out.println(String.join("   ",
-                " ***** ",
-                " ***** ",
-                " ******",
+                "*****  ",
                 " ***** "
-        ));
+        );
+
+        String line4 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        );
+
+        String line5 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "
+        );
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
